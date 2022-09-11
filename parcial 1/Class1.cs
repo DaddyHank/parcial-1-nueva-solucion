d6791048ea2014de9a0cecfd775ca9d94265e22f
elmag <elmag@DESKTOP-QHJH7K5>
2022-09-11 12:34:44 -0600

    Agregar archivos de proyecto.

---
+++ parcial 1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parcial_1
+{
+    class Class1
+    {
+        private string _nombre;
+        private int _edad;
+        private double _promedio;
+
+        public _Estudiantes(string nombre, int edad, double promedio)
+        {
+            _nombre = nombre;
+            _edad = edad;
+            _promedio = promedio;
+        }
+
+        internal bool getData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ parcial 1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+
+namespace parcial_1
+{
+    class Program
+    {
+        private static string Nombre;
+        private static int Edad;
+        private static double Promedio;
+
+        static void Main(string[] args)
+        {
+            string condicion = "si";
+            do
+            {
+                int Estudiantes;
+                ArrayList registro = new ArrayList();
+                string nombre;
+                int edad;
+                double lab1;
+                double lab2;
+                double parcial;
+                double promedio;
+                int contador = 0;
+
+                Console.WriteLine("Ingrese el número de estudiantes a procesar: ");
+                Estudiantes = int.Parse(Console.ReadLine());
+
+                while (contador < Estudiantes)
+                {
+                    Console.WriteLine("Ingrese el nombre del estudiante: ");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese la edad del estudiante: ");
+                    edad = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la nota del laboratorio 1: ");
+                    lab1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la nota del laboratorio 2: ");
+                    lab2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la nota del parcial: ");
+                    parcial = double.Parse(Console.ReadLine());
+                    promedio = (lab1 * 0.3) + (lab2 * 0.3) + (parcial * 0.4);
+
+                    _Estudiantes estudiante = new _Estudiantes(Nombre = nombre, Edad = edad, Promedio = promedio);
+                    registro.Add(estudiante);
+
+                    contador++;
+
+                    if (promedio >= 6)
+                    {
+                        Console.WriteLine("Aprobado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Reprobado");
+                    }
+                }
+
+                foreach (_Estudiantes st in registro)
+                {
+                    Console.WriteLine(st.getData());
+                }
+
+                Console.WriteLine("Si desea continuar ingresando datos digite 'si', sino, presione cualquier tecla");
+                condicion = Console.ReadLine();
+            } while (condicion == "si");
+        }
+    }
+}
